b8d6fa72139aab9e21245528896557b5ce154acc
Nemanja Nikolć <58153223+nexencode@users.noreply.github.com>
2020-07-12 14:51:16 +0200

    Add project files.

---
+++ WebShopLoginApp/AdminDashboard.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebShopLoginApp
+{
+    public partial class LogInForm : Form
+    {
+        public LogInForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=DESKTOP-77T8UC5\SQLEXPRESS;Initial Catalog=WebShopeDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            string query = $"select * from Users where Username ='" + usernameTxtBox.Text.Trim() + "'and Password = '" + passwordTxtBox.Text.Trim() + "'";
+
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+
+            if (dtbl.Rows.Count == 1)
+            {
+                frmAdminDashboard objFrmAdminDashboard = new frmAdminDashboard();
+                MessageBox.Show($"Uspesno se se ulogovali");
+                this.Hide();
+                objFrmAdminDashboard.Show();
+            }
+            else
+            {
+                MessageBox.Show($"Check username and password!");
+            }
+
+            //if (usernameTxtBox.Text.Trim() == "Nemanja" && passwordTxtBox.Text.Trim() == "123")
+            //{
+            //    frmAdminDashboard objFrmAdminDashboard = new frmAdminDashboard();
+            //    MessageBox.Show($"Uspesno se se ulogovali");
+            //    this.Hide();
+            //    objFrmAdminDashboard.Show();
+            //}
+            //else
+            //{ 
+            //    MessageBox.Show($"Check username and password!");
+            //}
+        }
+
+        private void btnExitLogin_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ WebShopLoginApp/LogIn.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace WebShopLoginApp
+{
+    partial class frmAdminDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAdminDashboard));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCreateUser = new System.Windows.Forms.Button();
+            this.btnUpdateUser = new System.Windows.Forms.Button();
+            this.btnDeleteUser = new System.Windows.Forms.Button();
+            this.btnCreateRole = new System.Windows.Forms.Button();
+            this.btnUpdateRole = new System.Windows.Forms.Button();
+            this.btnDeleteRole = new System.Windows.Forms.Button();
+            this.btnCreateProduct = new System.Windows.Forms.Button();
+            this.btnUpdateProduct = new System.Windows.Forms.Button();
+            this.btnDeleteProduct = new System.Windows.Forms.Button();
+            this.btnFilterUsers = new System.Windows.Forms.Button();
+            this.btnFilterProducts = new System.Windows.Forms.Button();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(659, 130);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(189, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ADMIN DASHBOARD";
+            // 
+            // btnCreateUser
+            // 
+            this.btnCreateUser.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnCreateUser.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCreateUser.Location = new System.Drawing.Point(98, 176);
+            this.btnCreateUser.Name = "btnCreateUser";
+            this.btnCreateUser.Size = new System.Drawing.Size(121, 44);
+            this.btnCreateUser.TabIndex = 1;
+            this.btnCreateUser.Text = "Create User";
+            this.btnCreateUser.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdateUser
+            // 
+            this.btnUpdateUser.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnUpdateUser.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnUpdateUser.Location = new System.Drawing.Point(98, 244);
+            this.btnUpdateUser.Name = "btnUpdateUser";
+            this.btnUpdateUser.Size = new System.Drawing.Size(121, 44);
+            this.btnUpdateUser.TabIndex = 2;
+            this.btnUpdateUser.Text = "Update User";
+            this.btnUpdateUser.UseVisualStyleBackColor = false;
+            // 
+            // btnDeleteUser
+            // 
+            this.btnDeleteUser.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnDeleteUser.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDeleteUser.Location = new System.Drawing.Point(98, 313);
+            this.btnDeleteUser.Name = "btnDeleteUser";
+            this.btnDeleteUser.Size = new System.Drawing.Size(121, 44);
+            this.btnDeleteUser.TabIndex = 3;
+            this.btnDeleteUser.Text = "Delete User";
+            this.btnDeleteUser.UseVisualStyleBackColor = false;
+            // 
+            // btnCreateRole
+            // 
+            this.btnCreateRole.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnCreateRole.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCreateRole.Location = new System.Drawing.Point(277, 176);
+            this.btnCreateRole.Name = "btnCreateRole";
+            this.btnCreateRole.Size = new System.Drawing.Size(121, 44);
+            this.btnCreateRole.TabIndex = 4;
+            this.btnCreateRole.Text = "Create Role";
+            this.btnCreateRole.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdateRole
+            // 
+            this.btnUpdateRole.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnUpdateRole.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnUpdateRole.Location = new System.Drawing.Point(277, 244);
+            this.btnUpdateRole.Name = "btnUpdateRole";
+            this.btnUpdateRole.Size = new System.Drawing.Size(121, 44);
+            this.btnUpdateRole.TabIndex = 5;
+            this.btnUpdateRole.Text = "Update Role";
+            this.btnUpdateRole.UseVisualStyleBackColor = false;
+            // 
+            // btnDeleteRole
+            // 
+            this.btnDeleteRole.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnDeleteRole.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDeleteRole.Location = new System.Drawing.Point(277, 313);
+            this.btnDeleteRole.Name = "btnDeleteRole";
+            this.btnDeleteRole.Size = new System.Drawing.Size(121, 44);
+            this.btnDeleteRole.TabIndex = 6;
+            this.btnDeleteRole.Text = "Delete Role";
+            this.btnDeleteRole.UseVisualStyleBackColor = false;
+            // 
+            // btnCreateProduct
+            // 
+            this.btnCreateProduct.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnCreateProduct.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCreateProduct.Location = new System.Drawing.Point(448, 176);
+            this.btnCreateProduct.Name = "btnCreateProduct";
+            this.btnCreateProduct.Size = new System.Drawing.Size(121, 44);
+            this.btnCreateProduct.TabIndex = 7;
+            this.btnCreateProduct.Text = "Create Product";
+            this.btnCreateProduct.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdateProduct
+            // 
+            this.btnUpdateProduct.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnUpdateProduct.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnUpdateProduct.Location = new System.Drawing.Point(448, 244);
+            this.btnUpdateProduct.Name = "btnUpdateProduct";
+            this.btnUpdateProduct.Size = new System.Drawing.Size(121, 44);
+            this.btnUpdateProduct.TabIndex = 8;
+            this.btnUpdateProduct.Text = "Update Product";
+            this.btnUpdateProduct.UseVisualStyleBackColor = false;
+            // 
+            // btnDeleteProduct
+            // 
+            this.btnDeleteProduct.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnDeleteProduct.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDeleteProduct.Location = new System.Drawing.Point(448, 313);
+            this.btnDeleteProduct.Name = "btnDeleteProduct";
+            this.btnDeleteProduct.Size = new System.Drawing.Size(121, 44);
+            this.btnDeleteProduct.TabIndex = 9;
+            this.btnDeleteProduct.Text = "Delete Product";
+            this.btnDeleteProduct.UseVisualStyleBackColor = false;
+            // 
+            // btnFilterUsers
+            // 
+            this.btnFilterUsers.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnFilterUsers.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnFilterUsers.Location = new System.Drawing.Point(98, 380);
+            this.btnFilterUsers.Name = "btnFilterUsers";
+            this.btnFilterUsers.Size = new System.Drawing.Size(121, 44);
+            this.btnFilterUsers.TabIndex = 10;
+            this.btnFilterUsers.Text = "Filter Users";
+            this.btnFilterUsers.UseVisualStyleBackColor = false;
+            // 
+            // btnFilterProducts
+            // 
+            this.btnFilterProducts.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnFilterProducts.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnFilterProducts.Location = new System.Drawing.Point(448, 380);
+            this.btnFilterProducts.Name = "btnFilterProducts";
+            this.btnFilterProducts.Size = new System.Drawing.Size(121, 44);
+            this.btnFilterProducts.TabIndex = 11;
+            this.btnFilterProducts.Text = "Filter Products";
+            this.btnFilterProducts.UseVisualStyleBackColor = false;
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.btnLogOut.FlatAppearance.BorderSize = 0;
+            this.btnLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogOut.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogOut.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnLogOut.Location = new System.Drawing.Point(291, 472);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(91, 37);
+            this.btnLogOut.TabIndex = 12;
+            this.btnLogOut.Text = "LogOut";
+            this.btnLogOut.UseVisualStyleBackColor = false;
+            // 
+            // frmAdminDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 534);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.btnFilterProducts);
+            this.Controls.Add(this.btnFilterUsers);
+            this.Controls.Add(this.btnDeleteProduct);
+            this.Controls.Add(this.btnUpdateProduct);
+            this.Controls.Add(this.btnCreateProduct);
+            this.Controls.Add(this.btnDeleteRole);
+            this.Controls.Add(this.btnUpdateRole);
+            this.Controls.Add(this.btnCreateRole);
+            this.Controls.Add(this.btnDeleteUser);
+            this.Controls.Add(this.btnUpdateUser);
+            this.Controls.Add(this.btnCreateUser);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmAdminDashboard";
+            this.Text = "Admin Dashboard";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCreateUser;
+        private System.Windows.Forms.Button btnUpdateUser;
+        private System.Windows.Forms.Button btnDeleteUser;
+        private System.Windows.Forms.Button btnCreateRole;
+        private System.Windows.Forms.Button btnUpdateRole;
+        private System.Windows.Forms.Button btnDeleteRole;
+        private System.Windows.Forms.Button btnCreateProduct;
+        private System.Windows.Forms.Button btnUpdateProduct;
+        private System.Windows.Forms.Button btnDeleteProduct;
+        private System.Windows.Forms.Button btnFilterUsers;
+        private System.Windows.Forms.Button btnFilterProducts;
+        private System.Windows.Forms.Button btnLogOut;
+    }
+}
