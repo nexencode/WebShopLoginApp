619c15bce0a496e4de9f7421821e18d6199a4cfe
Nemanja Nikolć <58153223+nexencode@users.noreply.github.com>
2020-07-22 18:51:44 +0200

    Connect Database

--- WebShopLoginApp/Forms/LogInForm.cs
+++ WebShopLoginApp/Forms/LogInForm.cs
@@ -21,38 +21,7 @@ namespace WebShopLoginApp
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            const string connectionString = @"Data Source=DESKTOP-77T8UC5\SQLEXPRESS;Initial Catalog=WebShopProjectFinal;Integrated Security=True";
-            SqlConnection sqlcon = new SqlConnection();
-
-            string query = $"select * from Users where Username ='" + usernameTxtBox.Text.Trim() + "'and Password = '" + passwordTxtBox.Text.Trim() + "'";
-
-            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
-            DataTable dtbl = new DataTable();
-            sda.Fill(dtbl);
-
-            if (dtbl.Rows.Count == 1)
-            {
-                frmAdminDashboard objFrmAdminDashboard = new frmAdminDashboard();
-                MessageBox.Show($"Uspesno se se ulogovali");
-                this.Hide();
-                objFrmAdminDashboard.Show();
-            }
-            else
-            {
-                MessageBox.Show($"Check username and password!");
-            }
-
-            //if (usernameTxtBox.Text.Trim() == "Nemanja" && passwordTxtBox.Text.Trim() == "123")
-            //{
-            //    frmAdminDashboard objFrmAdminDashboard = new frmAdminDashboard();
-            //    MessageBox.Show($"Uspesno se se ulogovali");
-            //    this.Hide();
-            //    objFrmAdminDashboard.Show();
-            //}
-            //else
-            //{ 
-            //    MessageBox.Show($"Check username and password!");
-            //}
+            
         }
 
         private void btnExitLogin_Click(object sender, EventArgs e)
